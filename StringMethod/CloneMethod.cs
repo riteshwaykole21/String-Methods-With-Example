53215deae468a9361c83458c3eed0e32ce4699c9
“riteshwaykole21” <riteshwaykole21@gmail.com>
2023-04-19 13:05:55 +0530

    [Ritesh]add : Git Clone Method

---
+++ StringMethod/CloneMethod.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringMethod
+{
+    public class CloneMethod
+    {
+        public void Method()
+        {
+            string a = "Hello";
+            string b = (string)a.Clone();
+            Console.WriteLine(b);
+        }
+    }
+}
---
+++ StringMethod/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringMethod
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("1 : Clone Method Example");
+            Console.WriteLine("Enter A Option");
+            int Option  = Convert.ToInt32(Console.ReadLine());
+            switch (Option)
+            {
+                case 1:
+                    CloneMethod cloneMethod = new CloneMethod();
+                    cloneMethod.Method();
+                    break;
+            }
+            Console.ReadLine();
+        }
+    }
+}
